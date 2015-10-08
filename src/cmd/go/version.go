a74441a43fc12d91807d799dc901dca03be11d8e
mapix <mapix.me@gmail.com>
2015-10-08 17:44:32 +0800

    add custom version string

--- src/cmd/go/version.go
+++ src/cmd/go/version.go
@@ -21,5 +21,5 @@ func runVersion(cmd *Command, args []string) {
 		cmd.Usage()
 	}
 
-	fmt.Printf("go version %s %s/%s\n", runtime.Version(), runtime.GOOS, runtime.GOARCH)
+	fmt.Printf("go version %s %s/%s Customed by Douban (Meta,Mirror,Branch)\n", runtime.Version(), runtime.GOOS, runtime.GOARCH)
 }
